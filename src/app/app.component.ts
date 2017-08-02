95bfe02f0ea5f0d4299c26cc357d2bcc3028503d
Donwor <diogo.g.c@gmail.com>
2017-08-02 13:07:51 +0100

    Minor Tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,52 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   
-
-  // variaveis de API
-    // variaveis para funcionamento
-  
-
-
-  // API Calls
+    
   constructor(private http: Http){
       this.GetHandWritings();
   }
 
-  handwriting_id: string = '';
-  text: string = '';
-  handwriting_size: string = '';  
-  handwriting_color: string = '';
-  finalData:any;
+  
+// API Calls
+    createAuthorizationHeader(headers: Headers) {
+    headers.append('Authorization', 'Basic ' +
+      btoa('0894436AGCWCAHXG:BKD252QSP91JJQ75')); 
+  }
+
+
+  GetHandWritings(){
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+     this.http.get(this.handWriteURL, {
+      headers:headers
+    })
+      .map(res => res.json())
+      .subscribe(data => {
+        this.data = data;
+        
+      })
+      console.log(this.data);
+  }
+
+  //input properties 
+  handwritingId: string;
+  text: string;
+  handwritingSize: string;
+  handwritingColor: string;
+
+  // API properties
   title = 'Handwriting API APP';
   apiKey: string = '0894436AGCWCAHXG';
   apiSecret: string = 'BKD252QSP91JJQ75';
-  handWriteURL: string = 'https://api.handwriting.io/handwritings?limit=20'
-  FinalURL: string ='https://api.handwriting.io/render/png?';
+  handWriteURL: string = 'https://api.handwriting.io/handwritings?limit=500'
+  apiURL: string ='https://api.handwriting.io/render/png?handwriting_id=2ZK3ZD280058&text=qwdqwdqwd&handwriting_color=692E98';
+
+  
+  // Method properties
+  finalData:string;
   data: any = [{}];
+
+  //arrays
   font_size:any = [
     {
       "name" : "Default",
@@ -74,40 +98,35 @@ export class AppComponent {
     }
   ]
 
-    createAuthorizationHeader(headers: Headers) {
-    headers.append('Authorization', 'Basic ' +
-      btoa('0894436AGCWCAHXG:BKD252QSP91JJQ75')); 
-  }
+  
 
-
-  GetHandWritings(){
-    let headers = new Headers();
-    this.createAuthorizationHeader(headers);
-     this.http.get(this.handWriteURL, {
-      headers:headers
-    })
-      .map(res => res.json())
-      .subscribe(data => {
-        this.data = data;
-        
-      })
-  }
-
-  Final: string;
-
-  RenderImage(){
-    let headers = new Headers();
-    this.createAuthorizationHeader(headers);
-     this.http.get(this.FinalURL
-      +'handwriting_id='+this.handwriting_id
+    RenderFinalURL(url: string){
+      url = this.apiURL
+      +'handwriting_id='+this.handwritingId
       +'&text='+this.text
-      +'&handwriting_size='+this.handwriting_size
-      +'&handwriting_color='+this.handwriting_color,{headers: headers}).subscribe(data => {this.Final = data.toString()});    
-      console.log(this.Final);
+      +'&handwriting_size='+this.handwritingSize
+      +'&handwriting_color='+this.handwritingColor;
+      console.log(url);
+      return url;
   }
+  
+  Imagem: any;
 
- 
+   RenderImage(imagem){
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    imagem = this.http.get(this.apiURL,{headers:headers});
+    return imagem;
+   }
+
+
+
+
+
     
+  
+
+   
 
  
 }
